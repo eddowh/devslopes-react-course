0205981e04cbf053c2a155cdfa74d99684de8180
Eddo Hintoso <eddo.hintoso@gmail.com>
2016-04-03 19:56:41 +0800

    Add more components to demonstrate reusability

--- src/components/ListManager.jsx
+++ src/components/ListManager.jsx
@@ -45,10 +45,20 @@ var ListManager = React.createClass({
             marginTop: 10
         };
 
+        var panelStyle = {};
+        var panelHeadingStyle = {};
+        var buttonStyle = {};
+
+        if (this.props.headingColor) {
+            panelHeadingStyle.background = this.props.headingColor;
+            panelStyle.borderColor = this.props.headingColor;
+            buttonStyle.background = this.props.headingColor;
+        }
+
         return (
             <div style={divStyle} className="col-sm-4">
-                <div className="panel panel-primary">
-                    <div className="panel-heading">
+                <div style={panelStyle} className="panel panel-primary">
+                    <div style={panelHeadingStyle} className="panel-heading">
                         <h3>
                             {this.props.title}
                         </h3>
@@ -59,13 +69,13 @@ var ListManager = React.createClass({
                                 <input className="form-control" onChange={this.onChange} value={this.state.newItemText} />
                             </div>
                             <div className="col-sm-3">
-                                <button className="btn btn-primary">
+                                <button style={buttonStyle} className="btn btn-primary">
                                     Add
                                 </button>
                             </div>
                         </form>
-                        <List items={this.state.items} />
                     </div>
+                    <List items={this.state.items} />
                 </div>
             </div>
         );
--- src/main.jsx
+++ src/main.jsx
@@ -8,4 +8,6 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 var ListManager = require('./components/ListManager.jsx')
 
-ReactDOM.render(<ListManager title="Ingredients" />, document.getElementById('ingredients'))
+ReactDOM.render(<ListManager title="Ingredients" headingColor="#DC143C" />, document.getElementById('ingredients'))
+ReactDOM.render(<ListManager title="ToDo" headingColor="#337ab7" />, document.getElementById('todo'))
+ReactDOM.render(<ListManager title="Christmas" headingColor="#2E8B57" />, document.getElementById('christmas'))
